b9b3cdb3b7a077f85c03a03e53b9bd2f29af5a2e
Gustavo Castillo <ga.castillo@uniandes.edu.co>
2022-09-07 04:03:34 -0500

    Fixed file structures

--- stores/scripts/2-cleaning.R
+++
@@ -1,6 +0,0 @@
-# Limpieza de datos
-## Limpiar personas empleadas (dsi==0) y mayores de 18 (age>=18)
-df <- read_csv("../stores/encuesta.csv")
-df <- geih %>% 
-  filter(age>=18, dsi==0) %>%
-  select(ingtotob, age, p6210, dsi, maxEducLevel,estrato1, sex)
